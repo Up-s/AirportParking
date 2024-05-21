d10da4fdcbbc68163ace4ad158b24c4e1aff8c74
Up-s <dldbdjq@gmail.com>
2024-05-22 04:22:03 +0900

    DELETE

--- airport_parking/lib/data/repository/firebase_api_repository_impl.dart
+++ airport_parking/lib/data/repository/firebase_api_repository_impl.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:airport_parking/data/data_source/firebase_api.dart';
 import 'package:airport_parking/domain/model/app_config.dart';
 import 'package:airport_parking/domain/respository/firebase_api_repository.dart';
